55bcf3b8ec8093e124e5deaabe6388c47927093f
-=SCH=- <YYakunin@gmail.com>
2022-07-21 16:29:01 +0300

    unit 2 function to shoose_date
    Signed-off-by: -=SCH=- <YYakunin@gmail.com>

--- RestrictTimeLogs.py
+++ RestrictTimeLogs.py
@@ -1,6 +1,7 @@
 # Библиотеки
 import os
 from datetime import date,datetime,timedelta
+import dateutil.relativedelta as rltd
 from tkinter import *
 
 # Переменные
@@ -11,45 +12,43 @@ datetimeFormat_InLog = "%d.%m.%Y %H:%M:%S"
 # Функции
 def get_all_dates(foldernamelog, dateFormat_file_name):
     """ Получить все даты файлов в папке с логами Даты содержатся в имени файлов  """
+
     listdates = []
     for file in os.listdir(foldernamelog):
         if file.endswith(".log"):
             filename = file.split(".")[0]
             datefile = datetime.strptime(filename, dateFormat_file_name)
             listdates.append(datefile)
+
     return listdates
 
-def get_start_stop_dates(list_all_dates):
-    """ Получаем начальные границы дат как последняя доступная минус 1 месяц """
-    last_date = list_all_dates[-1]
-    start_date = last_date - timedelta()
-    return start_date,last_date
+def choose_dates(list_all_dates):
+    """ Получаем границы дат как последняя доступная минус 1 месяц
+        Рисуем окно с выбором начальной и конечной даты для переназначения их по своему желанию
+    """
+    stop_date = list_all_dates[-1]
+    # start_date = stop_date - timedelta(days=30)
+    start_date = stop_date - rltd.relativedelta(months=1)
 
-def draw_select_dates(start_stop_dates):
-    """ Рисуем окно с выбором начальной и конечной даты """
-    start_date = start_stop_dates[0]
-    stop_date = start_stop_dates[1]
-
-    # Создаём окно
+    # Создаём окно в котором можно выбрать свои крайние даты для графика
     root = Tk()
 
 
+    return start_date, stop_date
 
-def get_data_for_diag():
+def get_data_from_files(start_stop_dates):
     """ Отбираем данные для диаграммы из файлов с требуемым диапазоном дат """
-    pass
+    return start_stop_dates
 
-def draw_diag():
+def draw_diag(time_content):
     """ Рисуем диаграмму по набору дат """
-    pass
+    print("Рисуем",time_content)
+
 
 #######################################################################
-print("Начали")
 
-list_all_dates = get_all_dates(foldernamelog,dateFormat_file_name)
-start_stop_dates = get_start_stop_dates(list_all_dates) # Получить начальные даты как текущая дата минус 30 дней
-draw_select_dates(start_stop_dates) # Рисуем окно с выбором начальной и конечной даты
-get_data_for_diag()# Получаем две даты и вытаскиваем из всех файлов этого периода время
-draw_diag() # Рисуем диаграмму по полученным временам.
+list_all_dates = get_all_dates(foldernamelog,dateFormat_file_name) # Получаем список всех дат
+start_stop_dates = choose_dates(list_all_dates) # Рисуем окно с выбором начальной и конечной даты
+time_content = get_data_from_files(start_stop_dates)# Получаем крайние даты и вытаскиваем из всех файлов этого периода время
+draw_diag(time_content) # Рисуем диаграмму по полученным временам.
 
-print("Кончили")
--- Test.py
+++ Test.py
@@ -3,5 +3,13 @@ print(datetime.strptime("2022-10-15", "%Y-%m-%d"))
 
 
 
+import dateutil.relativedelta as rltd
+d = datetime.strptime("2013-03-31", "%Y-%m-%d")
+d2 = d - rltd.relativedelta(months=1)
+print(d2)
+
+
+
+
 
 
