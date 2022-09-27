eeb97094a952be75ff0ef089e6189002bdc901dd
-=SCH=- <YYakunin@gmail.com>
2022-09-27 20:18:02 +0300

    Data complete need graph

--- RestrictTimeLogs.py
+++ RestrictTimeLogs.py
@@ -23,7 +23,7 @@ default_days = 2    # ?временный параметр кол-во дней 
 # Функции
 #######################################################################
 def get_all_dates(foldernamelog, dateFormat_file_name):
-    """ Получить все даты файлов в папке с логами Даты содержатся в имени файлов  """
+    """ Получить все даты файлов в папке с логами Даты содержатся в именах файлов  """
     listdates = []
     for file in os.listdir(foldernamelog):
         if file.endswith(".log"):
@@ -32,13 +32,13 @@ def get_all_dates(foldernamelog, dateFormat_file_name):
             listdates.append(datefile)
     return listdates
 
-def get_start_stop_dates(list_all_dates):
+def init_start_stop_dates(list_all_dates):
     """ Получаем начальные границы дат как последняя доступная минус что-то  
         (Пока не решил это будет фиксированное кол-во дней или минус 1 месяц)
     """
-    stop_date = list_all_dates[-1]  # последняя дата есть последняя дата в журнале
-    
-    # начальная дата диаграммы есть последняя минус default_days
+     # последняя дата есть последняя дата в журнале
+    stop_date = list_all_dates[-1] 
+    # начальная дата есть последняя минус default_days
     start_date = stop_date - timedelta(days=default_days)
     # ?или же будет  минус 1 месяц
     # start_date = stop_date - rltd.relativedelta(months=1)
@@ -46,7 +46,7 @@ def get_start_stop_dates(list_all_dates):
     return start_date, stop_date
     
 def get_times_from_files(foldernamelog, start_date, stop_date, dateFormat_file_name, datetimeFormatInFiles):
-    """ Получить данные времени из файлов с заданным диапазоном дат """
+    """ Получить список времён из файлов с заданным диапазоном дат """
     time_content = []
     for file in os.listdir(foldernamelog):
         if file.endswith(".log"):
@@ -56,10 +56,8 @@ def get_times_from_files(foldernamelog, start_date, stop_date, dateFormat_file_n
                
                 # Читаем строчки со временем из файла  в  time_content
                 with open( foldernamelog + "\\" + file, "r",  encoding='cp866') as fileobject:
-                   
                    # Флаг повторного значения минут
                    pred_min_remaind = 0 
-                   
                    # Разбираем строку как <Дата> <Час>:<Минута>:.....<Осталось минут>
                    for line in fileobject: 
                         # Осталось минут в день
@@ -75,17 +73,6 @@ def get_times_from_files(foldernamelog, start_date, stop_date, dateFormat_file_n
 
     return time_content
 
-def get_times_from_day(time_content, curday):
-    """ Получить список минут на конкретную дату """
-    times = []; minutes = []
-    for DT in time_content:
-            if DT.date() == curday:
-                t = DT.time()
-                m = t.hour * 60 + t.minute          # номер минуты от начала дня
-                times.append(t)
-                minutes.append(m)
-    return  times, minutes
-
 def parsed_timelist_to_string(listtimes):
     """ Преобразовать список дат или времён в текстовый вид """
     parsed_times = []
@@ -94,53 +81,66 @@ def parsed_timelist_to_string(listtimes):
         parsed_times.append(t_parsed)
     return parsed_times
 
-def draw_diag(time_content, start_date, stop_date):
-    """ Рисуем графики """
-    #num_days = (stop_date - start_date).days +1
-    num_days = (time_content - start_date).days +1
-    print( "Рисуем", num_days, "дн. с", start_date, "по", stop_date, ", Точек  =", len(time_content), "шт. ")
-  
-    # Получаем список дней,  списки времён посещения, списки минут дня и кол-во оставшихся минут в день
-    list_days = [];  list_times = []; list_minutes = []; list_txt_minutes = []; list_q_minutes = []
+def draw_diag(time_content):
+    """ Рисуем графики по списку времён """
+    start_date = time_content[0] 
+    stop_date = time_content[-1]
+    num_days = (stop_date - start_date).days +1
+    print( "Рисуем", num_days, "дн. с", start_date.date(), "по",stop_date.date(), ", Точек  =", len(time_content), "шт. ")
+      
+    # Список дней,  списки минут и оставшихся минут в день
+    list_days = [];  list_minutes = []; list_q_minutes = []
 
-    # Перебираем все 
-    # Перебираем день за днём от start_date до stop_date
-    for curday in (start_date + timedelta(n) for n in range(num_days)):
-        # получим из time_content  минуты дня и их кол-во 
-        list_times_day, list_minutes_day = get_times_from_day(time_content, curday) 
-        list_txt_times_day = parsed_timelist_to_string(list_times_day)
-        q_minutes_day = len(list_minutes_day)
-        # добавим полученное в списки
-        list_days.append(curday)
-        list_times.append(list_times_day)    
-        list_minutes.append(list_minutes_day)
-        list_txt_minutes.append(list_txt_times_day)
-        list_q_minutes.append(q_minutes_day)
-     
+    # Список дней по-быстрому (оставлено для образца)
+    #for curday in (start_date + timedelta(n) for n in range(num_days)):
+        #list_days.append(curday.date())
+
+    #  Бежим по контенту и формируем списки (одним проходом для ускорения)
+    curDate = time_content[0].date() 
+    curMinutes = []
+    
+    for curDT in  time_content:
+    
+        if curDT.date() == curDate:
+            t = curDT.time()  
+            m = t.hour * 60 + t.minute          # номер минуты от начала дня 
+            curMinutes.append(m)
+            continue
+        
+        # новый день, подводим итоги 
+        list_days.append(curDate)
+        list_minutes.append(curMinutes)
+        list_q_minutes.append(len(curMinutes)) 
+        # обнуляемся
+        curDate = curDT.date()
+        curMinutes = []
+    
+    # последний день тоже в списки
+    list_days.append(curDate)
+    list_minutes.append(curMinutes)
+    list_q_minutes.append(len(curMinutes)) 
+
+
     # ?: Думаем как будем выводить данные    
     print("Список дней", parsed_timelist_to_string(list_days))    
-    print("Минуты", list_txt_minutes)    
+    print("Минуты", list_minutes)    
     print("Q", list_q_minutes)
     
-    """ 
     # TODO: рисование
-        # fig, ax = plt.subplots()
-        # ax.set_title("Активность пользователя")
-        # fig.suptitle("График")
-        # #x = matd.date2num(datadays)
-        # x = datadays
-        # y = datatimes
-        
-        # ax.hist(datadays, bins = 50, rwidth = 0.4)
-
-        # plt.plot_date(x,y)
-        # plt.title("Активность пользователя")
-        # plt.xlabel("Даты")
-        # plt.ylabel("Время")
-        # plt.legend()    
-        # plt.gcf().autofmt_xdate()
-        # plt.show()
-    """
+    #    fig, ax = plt.subplots()
+    #    ax.set_title("Активность пользователя")
+    #    fig.suptitle("График")
+    #    x = matd.date2num(datadays)
+    #    x = datadays
+    #    y = datatimes
+    #    ax.hist(datadays, bins = 50, rwidth = 0.4)
+    #   plt.plot_date(x,y)
+    #   plt.title("Активность пользователя")
+    #    plt.xlabel("Даты")
+    #    plt.ylabel("Время")
+    #    plt.legend()    
+    #    plt.gcf().autofmt_xdate()
+    #    plt.show() 
 
 def press_ok():
     """ Обработчик кнопки "Нарисовать" главного окна.
@@ -151,8 +151,7 @@ def press_ok():
     # Читаем данные из файлов
     time_content = get_times_from_files(foldernamelog, start_date, stop_date, dateFormat_file_name, datetimeFormatInFiles) 
     # Рисуем контент
-    #print(time_content[-1], time_content[0])
-    draw_diag(time_content, start_date, stop_date)
+    draw_diag(time_content)
     
 #######################################################################
 # MAIN SCRIPT
@@ -164,7 +163,7 @@ def press_ok():
 list_all_dates = get_all_dates(foldernamelog, dateFormat_file_name)  
  
  # Получаем начальную и конечную даты для диаграммы вычислением от ближайшей минус что-то
-start_date, stop_date = get_start_stop_dates(list_all_dates)                
+start_date, stop_date = init_start_stop_dates(list_all_dates)                
 
 # Рисуем форму для корректировки диапазона дат с помощью виджета календаря 
 # и по кнопке "Нарисовать" запускаем рисование диаграммы через press_ok()
